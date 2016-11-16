e5cbf8682269386cc7ee47e5a433d4437f3b58c0
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2016-11-16 19:32:19 +0300

    Initial data commit

---
+++ ComputerVisionLab/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace ComputerVisionLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.effectsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.edgeDetectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dilateAndErodeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayscaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.metroTrackBar1 = new MetroFramework.Controls.MetroTrackBar();
+            this.metroTrackBar2 = new MetroFramework.Controls.MetroTrackBar();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.findContoursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.effectsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(645, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.openFileToolStripMenuItem.Text = "Open File...";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.W)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // effectsToolStripMenuItem
+            // 
+            this.effectsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.edgeDetectionToolStripMenuItem,
+            this.dilateAndErodeToolStripMenuItem,
+            this.grayscaleToolStripMenuItem,
+            this.findContoursToolStripMenuItem});
+            this.effectsToolStripMenuItem.Name = "effectsToolStripMenuItem";
+            this.effectsToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.effectsToolStripMenuItem.Text = "Effects";
+            // 
+            // edgeDetectionToolStripMenuItem
+            // 
+            this.edgeDetectionToolStripMenuItem.Name = "edgeDetectionToolStripMenuItem";
+            this.edgeDetectionToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.C)));
+            this.edgeDetectionToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.edgeDetectionToolStripMenuItem.Text = "Edge Detection";
+            this.edgeDetectionToolStripMenuItem.Click += new System.EventHandler(this.edgeDetectionToolStripMenuItem_Click);
+            // 
+            // dilateAndErodeToolStripMenuItem
+            // 
+            this.dilateAndErodeToolStripMenuItem.Name = "dilateAndErodeToolStripMenuItem";
+            this.dilateAndErodeToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.dilateAndErodeToolStripMenuItem.Text = "Dilate and Erode";
+            this.dilateAndErodeToolStripMenuItem.Click += new System.EventHandler(this.dilateAndErodeToolStripMenuItem_Click);
+            // 
+            // grayscaleToolStripMenuItem
+            // 
+            this.grayscaleToolStripMenuItem.Name = "grayscaleToolStripMenuItem";
+            this.grayscaleToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.grayscaleToolStripMenuItem.Text = "Grayscale";
+            this.grayscaleToolStripMenuItem.Click += new System.EventHandler(this.grayscaleToolStripMenuItem_Click);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.Location = new System.Drawing.Point(12, 79);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(618, 400);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // metroTrackBar1
+            // 
+            this.metroTrackBar1.BackColor = System.Drawing.Color.Transparent;
+            this.metroTrackBar1.CustomBackground = false;
+            this.metroTrackBar1.LargeChange = ((uint)(5u));
+            this.metroTrackBar1.Location = new System.Drawing.Point(12, 50);
+            this.metroTrackBar1.Maximum = 255;
+            this.metroTrackBar1.Minimum = 0;
+            this.metroTrackBar1.MouseWheelBarPartitions = 10;
+            this.metroTrackBar1.Name = "metroTrackBar1";
+            this.metroTrackBar1.Size = new System.Drawing.Size(264, 23);
+            this.metroTrackBar1.SmallChange = ((uint)(1u));
+            this.metroTrackBar1.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTrackBar1.StyleManager = null;
+            this.metroTrackBar1.TabIndex = 3;
+            this.metroTrackBar1.Text = "metroTrackBar1";
+            this.metroTrackBar1.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTrackBar1.Value = 20;
+            this.metroTrackBar1.ValueChanged += new System.EventHandler(this.metroTrackBar1_ValueChanged);
+            // 
+            // metroTrackBar2
+            // 
+            this.metroTrackBar2.BackColor = System.Drawing.Color.Transparent;
+            this.metroTrackBar2.CustomBackground = false;
+            this.metroTrackBar2.LargeChange = ((uint)(5u));
+            this.metroTrackBar2.Location = new System.Drawing.Point(330, 50);
+            this.metroTrackBar2.Maximum = 255;
+            this.metroTrackBar2.Minimum = 0;
+            this.metroTrackBar2.MouseWheelBarPartitions = 10;
+            this.metroTrackBar2.Name = "metroTrackBar2";
+            this.metroTrackBar2.Size = new System.Drawing.Size(264, 23);
+            this.metroTrackBar2.SmallChange = ((uint)(1u));
+            this.metroTrackBar2.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTrackBar2.StyleManager = null;
+            this.metroTrackBar2.TabIndex = 4;
+            this.metroTrackBar2.Text = "metroTrackBar2";
+            this.metroTrackBar2.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTrackBar2.Value = 70;
+            this.metroTrackBar2.ValueChanged += new System.EventHandler(this.metroTrackBar2_ValueChanged);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.CustomBackground = false;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Medium;
+            this.metroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Light;
+            this.metroLabel1.LabelMode = MetroFramework.Controls.MetroLabelMode.Default;
+            this.metroLabel1.Location = new System.Drawing.Point(12, 28);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(105, 19);
+            this.metroLabel1.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroLabel1.StyleManager = null;
+            this.metroLabel1.TabIndex = 5;
+            this.metroLabel1.Text = "Lower threshold:";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroLabel1.UseStyleColors = false;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.CustomBackground = false;
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Medium;
+            this.metroLabel2.FontWeight = MetroFramework.MetroLabelWeight.Light;
+            this.metroLabel2.LabelMode = MetroFramework.Controls.MetroLabelMode.Default;
+            this.metroLabel2.Location = new System.Drawing.Point(330, 28);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(107, 19);
+            this.metroLabel2.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroLabel2.StyleManager = null;
+            this.metroLabel2.TabIndex = 5;
+            this.metroLabel2.Text = "Upper threshold:";
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroLabel2.UseStyleColors = false;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.CustomBackground = false;
+            this.metroLabel3.FontSize = MetroFramework.MetroLabelSize.Medium;
+            this.metroLabel3.FontWeight = MetroFramework.MetroLabelWeight.Light;
+            this.metroLabel3.LabelMode = MetroFramework.Controls.MetroLabelMode.Default;
+            this.metroLabel3.Location = new System.Drawing.Point(282, 50);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(30, 19);
+            this.metroLabel3.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroLabel3.StyleManager = null;
+            this.metroLabel3.TabIndex = 5;
+            this.metroLabel3.Text = "255";
+            this.metroLabel3.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroLabel3.UseStyleColors = false;
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.CustomBackground = false;
+            this.metroLabel4.FontSize = MetroFramework.MetroLabelSize.Medium;
+            this.metroLabel4.FontWeight = MetroFramework.MetroLabelWeight.Light;
+            this.metroLabel4.LabelMode = MetroFramework.Controls.MetroLabelMode.Default;
+            this.metroLabel4.Location = new System.Drawing.Point(600, 50);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(30, 19);
+            this.metroLabel4.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroLabel4.StyleManager = null;
+            this.metroLabel4.TabIndex = 5;
+            this.metroLabel4.Text = "255";
+            this.metroLabel4.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroLabel4.UseStyleColors = false;
+            // 
+            // findContoursToolStripMenuItem
+            // 
+            this.findContoursToolStripMenuItem.Name = "findContoursToolStripMenuItem";
+            this.findContoursToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.findContoursToolStripMenuItem.Text = "Find Contours";
+            this.findContoursToolStripMenuItem.Click += new System.EventHandler(this.findContoursToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(645, 497);
+            this.Controls.Add(this.metroLabel4);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroTrackBar2);
+            this.Controls.Add(this.metroTrackBar1);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Canny";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem effectsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem edgeDetectionToolStripMenuItem;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private MetroFramework.Controls.MetroTrackBar metroTrackBar1;
+        private MetroFramework.Controls.MetroTrackBar metroTrackBar2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private System.Windows.Forms.ToolStripMenuItem dilateAndErodeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grayscaleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findContoursToolStripMenuItem;
+    }
+}
+
---
+++ ComputerVisionLab/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.Util;
+
+namespace ComputerVisionLab
+{
+    public partial class Form1 : Form
+    {
+        private Mat image;
+        private Mat outputImage;
+        private Canny cannyMethod;
+        private int minThreshold = 22;
+        private int maxThreshold = 46;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            imageBox1.FunctionalMode = ImageBox.FunctionalModeOption.PanAndZoom;
+            metroTrackBar1.Value = minThreshold;
+            metroTrackBar2.Value = maxThreshold;
+            metroLabel3.Text = metroTrackBar1.Value.ToString();
+            metroLabel4.Text = metroTrackBar2.Value.ToString();
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files | *.png; *.jpg; *.bmp; | All Files (*.*) | *.* ";
+            dialog.Title = "Open File...";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string filename = dialog.FileName;
+                image = CvInvoke.Imread(filename, Emgu.CV.CvEnum.LoadImageType.AnyColor);
+                SetWindowSize();
+                imageBox1.Image = image;
+                imageBox1.Refresh();
+                outputImage = new Mat();
+            }
+        }
+
+        private void edgeDetectionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RefreshCanny();
+        }
+
+        private void RefreshCanny()
+        {
+            if (image != null)
+            {
+                Mat destImage = new Mat();
+                image.CopyTo(destImage);
+                cannyMethod = new Canny(destImage, minThreshold, maxThreshold);
+                cannyMethod.getOutputImage().CopyTo(outputImage);
+                imageBox1.Image = outputImage;
+                //imageBox1.Image = cannyMethod.getOutputImage();
+                //imageBox1.Image = cannyMethod.getOutputImageOverSourceImage();
+                imageBox1.Refresh();
+            }
+        }
+
+        private void SetWindowSize()
+        {
+            if (image == null)
+                return;
+            Size maximumWindowSize = SystemInformation.MaxWindowTrackSize;
+            if (image.Width <= maximumWindowSize.Width - 42) // changing size of window depending on size of loaded image
+            {
+                if (image.Width < 650)
+                    Width = 650;
+                else
+                    Width = image.Width + 42;
+                imageBox1.Width = image.Width;
+            }
+            else
+            {
+                Width = maximumWindowSize.Width;
+                imageBox1.Width = Width - 42;
+            }
+            if (image.Height <= maximumWindowSize.Height - 128)
+            {
+                Height = image.Height + 128;
+                imageBox1.Height = image.Height;
+            }
+            else
+            {
+                Height = maximumWindowSize.Height - 60;
+                imageBox1.Height = Height - 128;
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog
+            {
+                Filter = @"JPG Image | *.jpg | PNG Image | *.png | Bitmap Image | *.bmp",
+                Title = @"Save As..."
+            };
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        imageBox1.Image.Bitmap.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case 2:
+                        imageBox1.Image.Bitmap.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                    case 3:
+                        imageBox1.Image.Bitmap.Save(fs, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                }
+                fs.Close();
+            };
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void metroTrackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            if (metroTrackBar1.Value <= maxThreshold - 1)
+            {
+                metroLabel3.Text = metroTrackBar1.Value.ToString();
+                minThreshold = metroTrackBar1.Value;
+                RefreshCanny();
+            }
+        }
+
+        private void metroTrackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            if (metroTrackBar2.Value >= minThreshold + 1)
+            {
+                metroLabel4.Text = metroTrackBar2.Value.ToString();
+                maxThreshold = metroTrackBar2.Value;
+                RefreshCanny();
+            }
+        }
+
+        private void dilateAndErodeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (image != null)
+            {
+                Canny dest = new Canny(image);
+                dest.Dilation(outputImage).CopyTo(outputImage);
+                imageBox1.Image = outputImage;
+                imageBox1.Refresh();
+            }
+        }
+
+        private void grayscaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (image != null)
+            {
+                Canny dest = new Canny(image);
+                Mat destImage = new Mat();
+                destImage = dest.Grayscale(outputImage);
+                if (destImage != null)
+                {
+                    destImage.CopyTo(outputImage);
+                    imageBox1.Image = outputImage;
+                    imageBox1.Refresh();
+                }
+            }
+        }
+
+        private void findContoursToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (image != null)
+            {
+                Canny dest = new Canny(image);
+                Mat destImage = new Mat();
+                destImage = dest.FindContours(outputImage);
+                if (destImage != null)
+                {
+                    destImage.CopyTo(outputImage);
+                    imageBox1.Image = outputImage;
+                    imageBox1.Refresh();
+                }
+            }
+        }
+    }
+}
